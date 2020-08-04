136bca73ef4479c89ddf1ef1f9fd59bdde905dcb
Jonathan Avila <jonathan.edav@gmail.com>
2020-08-04 15:01:01 -0600

    Update comments to consistent style

--- panel-test.cc
+++ panel-test.cc
@@ -24,56 +24,53 @@ static void InterruptHandler(int signal) {
   interrupt_received = true;
 }
 
-static void CyclePanels(Canvas *canvas, RGBMatrix::Options *options, int pause_s) {
+static void CyclePanels(RGBMatrix *matrix, RGBMatrix::Options *matrix_options) {
   Color colors[] = {color_red, color_green, color_blue};
 
-  int panel_cols = options->chain_length;
-  int panel_rows = options->parallel;
+  int panel_cols = matrix_options->chain_length;
+  int panel_rows = matrix_options->parallel;
 
-  // swap cols and rows if 'Rotate' parameter is 90 or 270
-  string mapping = options->pixel_mapper_config ? options->pixel_mapper_config : "";
+  // Swap panel_cols and panel_rows if 'Rotate' parameter is '90' or '270'.
+  string mapping = matrix_options->pixel_mapper_config ? matrix_options->pixel_mapper_config : "";
   if (!mapping.find("Rotate:90") || !mapping.find("Rotate:270"))
     swap(panel_cols, panel_rows);
 
-  // traverse panels row-wise
+  // Traverse panels row-wise.
   for (int col = 0; col < panel_cols; col++) {
     for (int row = 0; row < panel_rows; row++) {
 
-      // upper left (x0,y0) pixel of panel
-      int x0 = options->cols * col;
-      int y0 = options->rows * row;
+      int upper_x = matrix_options->cols * col;
+      int upper_y = matrix_options->rows * row;
+      int lower_x = matrix_options->cols * col + matrix_options->cols - 1;
+      int lower_y = matrix_options->rows * row + matrix_options->rows - 1;
 
-      // lower right (x1, y1) pixel of panel
-      int x1 = options->cols * col + options->cols - 1;
-      int y1 = options->rows * row + options->rows - 1;
-
-      // display colors individually on panel
+      // Display colors individually on panel.
       int n_colors = sizeof(colors) / sizeof(colors[0]);
       for (int i = 0; i < n_colors; i++) {
-        for (int x = x0; x <= x1; x++)
-          for (int y = y0; y <= y1; y++)
-          canvas->SetPixel(x, y, colors[i].r, colors[i].g, colors[i].b);
-	      sleep(pause_s);
+        for (int x = upper_x; x <= lower_x; x++)
+          for (int y = upper_y; y <= lower_y; y++)
+          matrix->SetPixel(x, y, colors[i].r, colors[i].g, colors[i].b);
+	      sleep(1);
       }
 
-      canvas->Clear();
+      matrix->Clear();
       
-      // display (<col>,<row>) on panel
+      // Display (<col>,<row>) on panel.
       char text[24];
       snprintf(text, sizeof(text), "(%d,%d)", col, row);
-      DrawText(canvas, font, x0, y1, color_white, &color_black, text);
-      sleep(pause_s);
+      DrawText(matrix, font, upper_x, lower_y, color_white, &color_black, text);
+      sleep(1);
 
-      canvas->Clear();
+      matrix->Clear();
     }
   }
 }
 
 int main(int argc, char *argv[]) {
-  RGBMatrix::Options options;
+  RGBMatrix::Options matrix_options;
   
-  Canvas *canvas = CreateMatrixFromFlags(&argc, &argv, &options);
-  if (canvas == NULL)
+  RGBMatrix *matrix = CreateMatrixFromFlags(&argc, &argv, &matrix_options);
+  if (matrix == NULL)
     return 1;
 
   const char *font_file = "fonts/6x12.bdf";
@@ -82,15 +79,15 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   
-  // set up signal handler for Ctrl+C presses
+  // Set handler for external interrupts.
   signal(SIGINT, InterruptHandler);
 
   cout << "Press Ctrl+C to exit.\n";
-  do {
-    CyclePanels(canvas, &options, 1);
-  } while (!interrupt_received);
+  do
+    CyclePanels(matrix, &matrix_options);
+  while (!interrupt_received);
 
-  delete canvas;
+  delete matrix;
 
   return 0;
 }
